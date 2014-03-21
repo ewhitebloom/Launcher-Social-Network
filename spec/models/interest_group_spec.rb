bdaa43df257d57e252a3079427712f3e39c57f6a
ewhitebloom <ewhitebloom@gmail.com>
2014-03-21 17:27:25 -0400

    specs

---
+++ spec/models/interest_group_spec.rb
@@ -0,0 +1,63 @@
+require 'spec_helper'
+
+describe InterestGroup do
+  let(:valid_attrs) { { name: 'Steve' } }
+
+  describe 'validations' do
+    context "when given valid attributes" do
+      it "is valid" do
+        interest_group = InterestGroup.new(valid_attrs)
+        expect(interest_group).to be_valid
+      end
+    end
+
+    it 'requires a name' do
+      # Create an InterestGroup using all of the valid attributes except replace
+      # the `name` key with a blank String.
+      interest_group = InterestGroup.new(valid_attrs.merge(name: ''))
+      expect(interest_group.errors[:name]).to include "can't be blank"
+    end
+
+    it 'requires a creator' do
+      # your code here
+    end
+
+    it 'optionally takes a description' do
+      # your code here
+    end
+  end
+
+  describe 'associations' do
+    it 'should have a creator' do
+    # create a User object
+    creator_attributes = {
+      first_name: "Barry",
+      last_name: "Zuckercorn",
+      email: "barry@hesverygood.com"
+    }
+    creator = User.create(creator_attributes)
+
+    # create an InterestGroup object with that user object as the creator
+    group_attributes = {
+      name: "Excuses",
+      creator: creator
+    }
+    group = InterestGroup.create(group_attributes)
+
+    expect(group.creator).to eq creator
+  end
+end
+
+describe '#number_of_posts' do
+  it 'returns the number of posts' do
+      # your code here for happy path test
+    end
+
+    it 'returns 0 if there are no posts' do
+      # your code here for boundary condition test
+    end
+
+    # etc.
+  end
+
+end
