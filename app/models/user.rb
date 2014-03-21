c3ed07b6fde9d6c330c4d043f7ade31987a9fc0d
ewhitebloom <ewhitebloom@gmail.com>
2014-03-21 16:23:38 -0400

    associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
-  belongs_to :interest_group
   has_many :posts
   has_many :comments
-  has_many :memberships
-  def self.memberships
-    @joins = self.joins(users: [{posts: :comments}])
+  has_many :interest_groups, through: :memberships
+
+  def memberships(user_id)
+    @joins = self.joins(:interest_groups)
+    @joins.select(:name).where(':user_id = user_id')
   end
 end
