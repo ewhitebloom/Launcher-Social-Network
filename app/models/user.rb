7f8c7d441a8632e6947f289490dd9790f14c5908
ewhitebloom <ewhitebloom@gmail.com>
2014-03-21 16:42:54 -0400

    comments and posts counts

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,14 @@ class User < ActiveRecord::Base
     user = User.where('id = user_id').first
     user.interest_groups
   end
+
+  def number_of_posts(user_id)
+    user = User.where('id = user_id').first
+    user.posts.count
+  end
+
+  def number_of_comments(user_id)
+    user = User.where('id = user_id').first
+    user.comments.count
+  end
 end
