7a26f08879a4a24a0f971e75e8cd6647a54c03f8
ewhitebloom <ewhitebloom@gmail.com>
2014-03-24 10:40:00 -0400

    spaces

--- spec/models/interest_group_spec.rb
+++ spec/models/interest_group_spec.rb
@@ -37,26 +37,26 @@ describe InterestGroup do
       }
       creator = User.create(creator_attributes)
 
-    group_attributes = {
-      name: "Excuses",
-      user: creator
-    }
-    group = InterestGroup.create(group_attributes)
+      group_attributes = {
+        name: "Excuses",
+        user: creator
+      }
+      group = InterestGroup.create(group_attributes)
 
-    expect(group.user).to eq creator
-  end
-end
-
-describe '#number_of_posts' do
-  it 'returns the number of posts' do
-    group = InterestGroup.new(valid_attrs)
-    Post.new({user_id: 1, title: 'hi', body: 'hi', date: Time.now, group_id: group.id })
-    expect(group.number_posts).to eq 1
+      expect(group.user).to eq creator
+    end
   end
 
-  it 'returns 0 if there are no posts' do
-    group = InterestGroup.create(valid_attrs)
-    expect(group.number_posts).to eq 0
+  describe '#number_of_posts' do
+    it 'returns the number of posts' do
+      group = InterestGroup.new(valid_attrs)
+      Post.new({user_id: 1, title: 'hi', body: 'hi', date: Time.now, group_id: group.id })
+      expect(group.number_posts).to eq 1
+    end
+
+    it 'returns 0 if there are no posts' do
+      group = InterestGroup.create(valid_attrs)
+      expect(group.number_posts).to eq 0
+    end
   end
-end
 end
